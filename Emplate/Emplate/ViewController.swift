a6d380b8783bccf3e43ecdc54197d546c40d87d8
AhmedMadian <ahmed.mostafa.madian@gmail.com>
2019-12-24 14:34:03 +0200

    init Project

---
+++ Emplate/Emplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emplate
+//
+//  Created by Ahmed Madian on 12/24/19.
+//  Copyright © 2019 Ahmed Madian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
